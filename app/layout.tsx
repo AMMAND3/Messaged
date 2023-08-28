9856f898d0075b51bc7b8d63a94959eb88d1a9a5
AMMAND3 <amman1@ualberta.ca>
2023-08-28 02:05:51 -0600

    First commite with Social Login UI

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import ToasterContext from './context/ToasterContext'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Messaged',
+  description: 'Message your peers!',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ToasterContext/>
+        {children}
+        {/* simple */}
+      </body>
+    </html>
+  )
+}
