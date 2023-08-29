8fcc91c9aef5f1f3d2dca3546e401f420a0ed048
AMMAND3 <amman1@ualberta.ca>
2023-08-28 22:52:52 -0600

    Layout for message platform

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import ToasterContext from './context/ToasterContext'
+import AuthContext from './context/AuthContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,9 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <AuthContext>
         <ToasterContext/>
         {children}
-        {/* simple */}
+        </AuthContext>
       </body>
     </html>
   )
