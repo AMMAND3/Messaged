e4ca3f7385754584f8391101c2dbfd52c7ee8a9c
AMMAND3 <amman1@ualberta.ca>
2023-09-06 17:12:50 -0600

    reverting to earlier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,11 @@
 import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import ToasterContext from './context/ToasterContext'
 import AuthContext from './context/AuthContext'
 import ActiveStatus from './components/ActiveStatus'
+import ToasterContext from './context/ToasterContext'
 
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata: Metadata = {
+export const metadata = {
   title: 'Messaged',
-  description: 'Message your peers!',
+  description: 'Messenge your peers!',
 }
 
 export default function RootLayout({
@@ -19,11 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <AuthContext>
-        <ToasterContext/>
-        <ActiveStatus/>
-        {children}
+          <ToasterContext />
+          <ActiveStatus />
+          {children}
         </AuthContext>
       </body>
     </html>
